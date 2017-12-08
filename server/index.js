e239e56e4a6dba5ad447a59a1ba6be552cedf830
Stuart Taylor <staylor1012@gmail.com>
2017-12-08 19:36:27 +0000

    Allow for production/development routing

--- server/index.js
+++ server/index.js
@@ -6,21 +6,27 @@ const handleStripe = require('./paymentHandlers/stripe');
 
 const port = config.port || 8080;
 
+const isProduction = process.env.NODE_ENV === 'production';
+
 const app = express();
 
 app.use(express.static('public'));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-app.get('/stripe-key', (req, res) => {
+function isProductionRoute(route) {
+  return isProduction ? route : `/api${route}`;
+}
+
+app.get(isProductionRoute('/stripe-key'), (req, res) => {
   res.json({ key: keys.stripe.public });
 });
 
-app.get('/amazon-key', (req, res) => {
+app.get(isProductionRoute('/amazon-key'), (req, res) => {
   res.json({ key: keys.amazon.merchant });
 });
 
-app.post('/charge-stripe', handleStripe);
+app.post(isProductionRoute('/charge-stripe'), handleStripe);
 
 app.listen(port, () => {
   console.log(`donate-api started on :${port}`);
